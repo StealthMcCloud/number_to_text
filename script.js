f50363212412dd61ae8df91d9ff06b817e874999
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-07 15:18:50 -0400

    First work

---
+++ script.js
@@ -0,0 +1,9 @@
+const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+const tens = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const hundreds = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const thousands = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+for (let i = 0; i <= 20; i++) {
+    result1 = ones[i];
+    console.log(result1); 
+}
